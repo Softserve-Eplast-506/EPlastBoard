6502d7241b55370e6db11faf05dec9c1eda3060b
Omeliukh-Nazar <91065958+Omeliukh-Nazar@users.noreply.github.com>
2022-04-20 23:00:32 +0300

    Board repository

---
+++ EPlastBoard/EPlastBoard.DAL/Repositories/Realization/BoardRepository.cs
@@ -0,0 +1,18 @@
+﻿using EPlastBoard.DAL.Entities;
+using EPlastBoard.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPlastBoard.DAL.Repositories.Realization
+{
+    public class BoardRepository : RepositoryBase<Board>, IBoardReapository
+    {
+        public BoardRepository(EPlastBoardDBContext dBContext)
+            : base(dBContext) 
+        {
+        }
+    }
+}
